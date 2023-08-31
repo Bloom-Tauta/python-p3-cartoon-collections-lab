5b14e231b97a9309210e1257b156d9f72a64966c
Bloom <bloomtauta@gmail.com>
2023-08-31 15:30:44 +0300

    done

--- lib/cartoon_collections.py
+++ lib/cartoon_collections.py
@@ -1,11 +1,18 @@
-def roll_call_dwarves():
-    pass
+def roll_call_dwarves(list):
+    for index, n in enumerate(list, start=1):
+        print(f"{index}. {n}")
 
-def summon_captain_planet():
-    pass
+def summon_captain_planet(list):
+    return [name.capitalize() + "!" for name in list]
 
-def long_planeteer_calls():
-    pass
+def long_planeteer_calls(list):
+    if len(list) == 4:
+        return True
+    elif len(list) >= 4:
+        return False
 
-def find_the_cheese():
-    pass
+def find_the_cheese(list):
+    cheese_list = ["cheddar", "gouda", "camembert"]
+    for cheese in list:
+        if cheese in cheese_list:
+            return cheese
